7d048cb07ad6c66efa4ce2bbb2d91407fb7baa84
belzinn <teo.belgrowicz@escola.pr.gov.br>
2025-09-30 09:52:47 -0300

    ef\saeg

---
+++ bel/src/teste/testejanela.java
@@ -0,0 +1,12 @@
+package teste;
+
+import javax.swing.JFrame;
+
+public class testejanela {
+    public static void main(String[] args) {
+        JFrame janela = new JFrame("bel");
+        janela.setBounds(0,0,1920,1080);
+        janela.setVisible(true);
+    }
+    
+}
