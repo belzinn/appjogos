f4efc42119b5af5177227360a66ce4b6fde19e29
belzinn <teo.belgrowicz@escola.pr.gov.br>
2025-10-07 09:50:43 -0300

    drgh
    dg

--- bel/src/interface/janela.java
+++ bel/src/interface/janela.java
@@ -1,4 +1,9 @@
-package interface;
+package
+import java.time.chrono.JapaneseDate;
+import javax.swing.JFrame;
+
+ interface;
+import java.security.Principal;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -10,6 +15,12 @@ public class janela extends JFrame{
         JPanel telas = new JPanel(controleTela);
         LoginPanel LoginPanel = new LoginPanel(telas, this);
         principalPanel principalPanel = new PrincipalPanel(telas, this);
+
+        this.add(telas);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.setUndecorated(true);
+        this.setVisible(true);
     }
     
 }
---
+++ bel/src/interface/loginPanel.java
@@ -0,0 +1,13 @@
+
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class loginPanel extends TelaPanel {
+    private JButton botaologin;
+    public loginPanel(JPanel telas, JFrame janela){
+        
+    }
+    
+}
